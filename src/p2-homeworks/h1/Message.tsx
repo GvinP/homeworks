177ae4db34417fd7bf59c99df60f8f8d2ad67c16
GvinP <92249588+GvinP@users.noreply.github.com>
2022-03-15 20:07:34 +0400

    add files

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -11,13 +11,13 @@ type messageProps = {
 function Message(props: messageProps) {
     return (
         <div className={s.message}>
-            <img className={s.img} src={props.avatar}></img>yar
-            <div className={s.meesageBody}>
+            <img className={s.img} src={props.avatar} alt={'avatar'}/>
+            <div className={s.messageBody}>
                 <div>
-                    <div>{props.name}</div>
-                    <div>{props.message}</div>
+                    <div className={s.name}>{props.name}</div>
+                    <div className={s.messageText}>{props.message}</div>
                 </div>
-                <div>{props.time}</div>
+                <div className={s.time}>{props.time}</div>
             </div>
         </div>
     )
